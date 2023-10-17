d54aaf84732f29927a3d79147f5dab7bed0af235
Denny Doan <denny-333@hotmail.com>
2023-10-17 16:15:02 -0400

    Added mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser, { BodyParser } from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -9,18 +9,57 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// CONFIGURATIONS
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Load environment variables from .env file
 dotenv.config();
+
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
-app.use(bodyParser.apply.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: 'true' }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true })); // Fixed 'extended' value
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
+// FILE STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Define the MongoDB connection URL
+const mongoURL = process.env.MONGO_URL;
+
+// Define the port for your MongoDB server
+const port = process.env.PORT || 6001;
+
+// Create the MongoDB connection string
+console.log(mongoURL);
+
+// Check if mongoURL is defined and attempt to connect
+if (mongoURL) {
+  mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => {
+      console.log(`Connected to MongoDB Port: ${port}`);
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+} else {
+  console.error('MONGO_URL is not defined. Make sure to set it in your .env file.');
+}
